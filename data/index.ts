4ffcc36adcc606736aa6671d8be8e3be01221304
spoofer <spoofer@deds3c.eu>
2025-04-11 15:49:23 +0100

    added home to navbar

--- data/index.ts
+++ data/index.ts
@@ -1,4 +1,5 @@
 export const navItems = [
+  { name: "Home", link: "/" },
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects-section" },
   { name: "Experience", link: "#experience-section" },
